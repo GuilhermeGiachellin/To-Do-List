9d6afeacbf89533fa37a59e8ddb8d78adc0288ba
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-07-14 18:56:54 -0300

    Linters

--- src/index.js
+++ src/index.js
@@ -3,31 +3,30 @@ import './style.css';
 const container = document.getElementById('container_ul');
 
 const list = [{
-    description: 'wash the dishes',
-    completed: false,
-    index: 1,   
+  description: 'wash the dishes',
+  completed: false,
+  index: 1,
 }, {
-    description: 'buy milk',
-    completed: false, 
-    index: 2,
+  description: 'buy milk',
+  completed: false,
+  index: 2,
 }, {
-    description: 'clean the floor',
-    completed: false, 
-    index: 3,
-}
+  description: 'clean the floor',
+  completed: false,
+  index: 3,
+},
 ];
-console.log(list.length)
+console.log(list.length);
 function component() {
-    for(let i = 0; i < list.length; i+=1){
-    const li = document.createElement('li');         
+  for (let i = 0; i < list.length; i += 1) {
+    const li = document.createElement('li');
     li.innerHTML = `<div>
-                    <input type="checkbox" id="task" name="tasks">                            
+                    <input type="checkbox" id="task" name="tasks">
                     <label for="tasks">${list[i].description}</label>
                     </div>
-                    <div><i class="fas fa-ellipsis-v"></i></i></div>`  
-    li.classList.add('tasks_li');                                      
-    container.appendChild(li)      
-    }
+                    <div><i class="fas fa-ellipsis-v"></i></i></div>`;
+    li.classList.add('tasks_li');
+    container.appendChild(li); 
   }
-
+}
 component();
