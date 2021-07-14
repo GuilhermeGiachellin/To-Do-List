e76cf6e0437f8322b4e6319afd802f8c548f41ee
Guilherme Giachellin <81584449+GuilhermeGiachellin@users.noreply.github.com>
2021-07-14 18:32:12 -0300

    List generate via Js
    -Added styling to the list
    -The list is fed with an array of objects

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import _ from 'lodash';
 import './style.css';
 
 const container = document.getElementById('container_ul');
 
 const list = [{
     description: 'wash the dishes',
-    completed: false,    
+    completed: false,
+    index: 1,   
 }, {
     description: 'buy milk',
     completed: false, 
+    index: 2,
 }, {
     description: 'clean the floor',
     completed: false, 
+    index: 3,
 }
 ];
-
+console.log(list.length)
 function component() {
-    const li = document.createElement('li');  
-   
-    container.innerHTML = ``
-   
-  
-    return element;
+    for(let i = 0; i < list.length; i+=1){
+    const li = document.createElement('li');         
+    li.innerHTML = `<div>
+                    <input type="checkbox" id="task" name="tasks">                            
+                    <label for="tasks">${list[i].description}</label>
+                    </div>
+                    <div><i class="fas fa-ellipsis-v"></i></i></div>`  
+    li.classList.add('tasks_li');                                      
+    container.appendChild(li)      
+    }
   }
-  
-  document.body.appendChild(component());
+
+component();
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,9 @@ module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'bundle.js',
+    filename: 'index.bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
